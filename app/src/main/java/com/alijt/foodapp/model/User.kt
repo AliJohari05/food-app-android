6b5f49185db0cb21a7a4da114b378f3c60bd0faa
AliJohari05 <alitehranijohari1384@gmail.com>
2025-07-13 12:15:36 +0330

    complete

--- app/src/main/java/com/alijt/foodapp/model/User.kt
+++ app/src/main/java/com/alijt/foodapp/model/User.kt
@@ -2,9 +2,14 @@ package com.alijt.foodapp.model
 
 data class User(
     val id: String,
-    val full_name: String,
     val phone: String,
     val email: String?,
+    val address: String,
     val role: String,
-    val address: String
+    val fullName: String,
+    val profileImageUrl: String? = null,
+    val status: String? = null,
+    val bank_info: BankInfo? = null,
+    val wallet_balance: Double? = null,
+    val profileImageBase64: String? = null
 )
